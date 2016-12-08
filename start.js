6956b46662a6745a29163a08e869e93596d341a2
Agus Sentosa <Agus Sentosa>
2016-12-08 21:15:13 +0700

    Change the number of shards to match the number of CPU

--- start.js
+++ start.js
@@ -1,11 +1,20 @@
 require('console-stamp')(console, '[yyyy-mm-dd HH:MM:ss.l]');
 
+var os = require('os');
+
 console.log("INIT MANAGER");
 
 require('./nconf_setup.js')().then(() => {
 	var discord = require("discord.js");
+
+	var coreNum = os.cpus().length;
+	console.log(coreNum + " core(s) detected");
+
+	if(coreNum < 1)
+		coreNum = 1;
+
 	// Create a new manager and spawn 2 shards
-	var manager = new discord.ShardingManager("./bot.js",1,true);
+	var manager = new discord.ShardingManager("./bot.js", coreNum, true);
 	manager.spawn();
 }).catch(() => {
 	console.error("nconf setup error");
